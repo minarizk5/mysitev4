7e7a277930673f2341af8d7efbf18bd20f9c860c
minarizk5 <minarizk8324@outlook.com>
2025-02-22 23:15:59 +0200

    v2

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Enable static exports for Netlify Edge Runtime
   output: 'export',
+};
 
-  // Images configuration for Netlify
-  images: {
-    unoptimized: true,
-  },
-
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
